12162e0d1355b3d74c5dde6cb79ddb9651e3e515
Peace Da <peace_da@nj-peace-da.client.tw.trendnet.org>
2015-01-09 13:47:51 +0000

    rm test class

--- app/src/main/java/org/peace/gradleandroidtemplate/ui/MainActivity.java
+++ app/src/main/java/org/peace/gradleandroidtemplate/ui/MainActivity.java
@@ -3,6 +3,7 @@ package org.peace.gradleandroidtemplate.ui;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import org.peace.gradleandroidtemplate.app.R;
+import org.peace.gradleandroidtemplate.util.LogHelper;
 
 
 public class MainActivity extends ActionBarActivity {
