01ad0cc9c9e97fa5cd10818ecc4bad26a66924ee
peacepassion <daqi1990@gmail.com>
2016-03-23 15:04:40 +0800

    simple test

---
+++ app.mine/src/main/java/net/wequick/example/small/app/mine/MainFragment.java
@@ -0,0 +1,65 @@
+package net.wequick.example.small.app.mine;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * Created by galen on 15/11/12.
+ */
+public class MainFragment extends Fragment {
+
+    private static final int REQUEST_CODE_COLOR = 1000;
+    private static final int REQUEST_CODE_CONTACTS = 1001;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView tvSection = (TextView) rootView.findViewById(R.id.section_label);
+        tvSection.setText(R.string.hello);
+        tvSection.setTextColor(getResources().getColor(R.color.abc_background_cache_hint_selector_material_dark));
+
+        Button button = (Button) rootView.findViewById(R.id.inter_start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainFragment.this.getContext(), PickerActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_COLOR);
+            }
+        });
+
+        button = (Button) rootView.findViewById(R.id.call_system_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, REQUEST_CODE_CONTACTS);
+            }
+        });
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != Activity.RESULT_OK) return;
+
+        switch (requestCode) {
+            case REQUEST_CODE_COLOR:
+                Button button = (Button) getView().findViewById(R.id.inter_start_button);
+                button.setText(getText(R.string.inter_start) + ": " + data.getStringExtra("color"));
+                break;
+            case REQUEST_CODE_CONTACTS:
+                break;
+        }
+    }
+}
--- app/src/main/java/org/peace/allinone/ui/MainActivity.java
+++ app/src/main/java/org/peace/allinone/ui/MainActivity.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
 import org.peace.allinone.R;
+
+import net.wequick.small.Small;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +23,21 @@ public class MainActivity extends AppCompatActivity {
     ButterKnife.inject(this);
   }
 
+  @Override protected void onStart() {
+    super.onStart();
+
+    Small.setBaseUri("http://m.wequick.net/demo/");
+    Small.setUp(this, new Small.OnCompleteListener() {
+      @Override public void onComplete() {
+        Toast.makeText(MainActivity.this, "set up OK", Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
+
   @OnClick({ R.id.start_btn }) public void onClick(View v) {
     int id = v.getId();
     if (id == R.id.start_btn) {
-
+      Small.openUri("mine", this);
     }
   }
 }
