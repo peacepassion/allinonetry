f06e5003a19a23314a8f649d26bfc3eddda0eec9
peacepassion <daqi1990@gmail.com>
2016-03-28 09:55:27 +0800

    test OK

--- app.mine/src/main/java/net/wequick/example/small/app/mine/MainActivity.java
+++ app.mine/src/main/java/net/wequick/example/small/app/mine/MainActivity.java
@@ -1,15 +1,15 @@
 package net.wequick.example.small.app.mine;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by galen on 15/11/11.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mine_main);
     }
 }
--- app/src/main/java/org/peace/allinone/MyApp.java
+++ app/src/main/java/org/peace/allinone/MyApp.java
@@ -2,6 +2,7 @@ package org.peace.allinone;
 
 import android.app.Application;
 import me.ele.commons.AppLogger;
+import net.wequick.small.Small;
 
 /**
  * Created by peacepassion on 15/8/11.
@@ -11,5 +12,7 @@ public class MyApp extends Application {
   @Override public void onCreate() {
     super.onCreate();
     AppLogger.debug = true;
+
+    Small.preSetUp(this);
   }
 }
