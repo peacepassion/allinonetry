d2dea891b4b36760bc9fd194c32b5933cb1b75de
peacepassion <daqi1990@gmail.com>
2016-05-28 11:14:29 +0800

    show popup on the rv item

--- app/src/main/java/org/peace/allinone/ui/FoodListAdapter.java
+++ app/src/main/java/org/peace/allinone/ui/FoodListAdapter.java
@@ -1,14 +1,24 @@
 package org.peace.allinone.ui;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
+import me.ele.commons.AppLogger;
 import me.ele.commons.CollectionsUtils;
 import org.peace.allinone.R;
+
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
 public class FoodListAdapter extends RecyclerView.Adapter<RVVH>
     implements PinnedItemDecoration.PinnedHeaderAdapter {
@@ -53,9 +63,32 @@ public class FoodListAdapter extends RecyclerView.Adapter<RVVH>
     });
     if (obj instanceof FoodCategory) {
       vh.bind(((FoodCategory) obj).name);
+      vh.itemView.setOnClickListener(new View.OnClickListener() {
+        @Override public void onClick(View v) {
+          //Toast.makeText(v.getContext(), "category: " + ((FoodCategory) obj).name, Toast.LENGTH_SHORT).show();
+          popup(vh.itemView.getContext(), v);
+        }
+      });
       return;
     }
     vh.bind((String) obj);
+  }
+
+  private void popup(Context context, View anchor) {
+    View contentView = LayoutInflater.from(context).inflate(R.layout.popup_content, null);
+    TextView titleView = (TextView) contentView.findViewById(R.id.title);
+    TextView content = (TextView) contentView.findViewById(R.id.content);
+    titleView.setText("Title");
+    content.setText("content");
+    PopupWindow popupWindow = new PopupWindow(contentView, 600, WRAP_CONTENT);
+    popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    popupWindow.setOutsideTouchable(true);
+    popupWindow.setFocusable(true);
+    popupWindow.setSoftInputMode(PopupWindow.INPUT_METHOD_NOT_NEEDED);
+    int[] loc = new int[2];
+    anchor.getLocationInWindow(loc);
+    AppLogger.e("x: " + loc[0] + ", y: " + loc[1]);
+    popupWindow.showAtLocation(anchor, Gravity.TOP | Gravity.RIGHT, 00, loc[1]);
   }
 
   @Override public int getItemViewType(int position) {
